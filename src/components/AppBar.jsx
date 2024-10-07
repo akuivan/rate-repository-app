80a847c689007e9e98c728a324fcc40ae152680a
akuivan <akuivan@users.noreply.github.com>
2024-10-07 11:05:37 +0300

    Exercise 10.7 done

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from './theme';
 import routes from './routes';
@@ -7,18 +7,23 @@ import AppBarTab from './AppBarTap';
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: theme.colors.backgroundAppBar,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    backgroundColor: theme.colors.backgroundAppBar,   
     padding: 10,
+  },
+  tabContainer: {
+    flexDirection: 'row',
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab label="Repositories" to={routes.home} />
-      <AppBarTab label="Sign In" to={routes.signIn} />
+      <ScrollView horizontal>
+        <View style={styles.tabContainer}>
+          <AppBarTab label="Repositories" to={routes.home} />
+          <AppBarTab label="Sign In" to={routes.signIn} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
